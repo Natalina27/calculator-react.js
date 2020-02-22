927cc8d44aa5eec017f63fcb6e43e0d850566df1
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-22 12:04:00 +0100

    Updates

---
+++ precache-manifest.5962a4493b36a611ed2a70cd283fa5a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7335a366b496eee35813d1f02488103",
+    "url": "/calculator-react.js/index.html"
+  },
+  {
+    "revision": "28d40b6aed4366b810de",
+    "url": "/calculator-react.js/static/css/main.6ae154c5.chunk.css"
+  },
+  {
+    "revision": "ff34087a131ea204991a",
+    "url": "/calculator-react.js/static/js/2.b5582bc4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator-react.js/static/js/2.b5582bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28d40b6aed4366b810de",
+    "url": "/calculator-react.js/static/js/main.6ac3661c.chunk.js"
+  },
+  {
+    "revision": "36bb088665ff8fb92c30",
+    "url": "/calculator-react.js/static/js/runtime-main.ab265bc4.js"
+  }
+]);
